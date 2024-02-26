aebee75ad801a3ccc81f504c109bcbbe9e9200d1
Prosenjit Manna <prosenjit@itobuz.com>
2024-02-26 13:00:47 +0530

    graphql sandbox added

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -1,7 +1,17 @@
 module.exports = [
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "frame-src": [ "http://localhost:*", "self", "sandbox.embed.apollographql.com" ],
+        },
+      },
+    },
+  },
+
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- api/config/plugins.js
+++ api/config/plugins.js
@@ -11,4 +11,11 @@ module.exports = () => ({
       },
     },
   },
+  "apollo-sandbox": {
+    // enables the plugin only in development mode
+    // if you also want to use it in production, set this to true
+    // keep in mind that graphql playground has to be enabled
+    enabled: process.env.NODE_ENV === "production" ? false : true,
+    // endpoint: "http://localhost:1337/graphql", // OPTIONAL - endpoint has to be accessible from the browser
+  },
 });
